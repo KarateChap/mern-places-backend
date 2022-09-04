3f3f3e5ec1f6fb147365c5c91967c5887e9a0b3b
Ralph Gabriel Mariano <57673613+KarateChap@users.noreply.github.com>
2022-09-04 21:29:43 +0800

    connecting backend to mongodb

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-routes");
-const usersRoutes = require('./routes/users-routes');
+const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
-
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route.', 404);
+  const error = new HttpError("Could not find this route.", 404);
   throw error;
 });
 
@@ -23,7 +23,14 @@ app.use((error, req, res, next) => {
     return next(error);
   }
   res.status(error.code || 500);
-  res.json({message: error.message || 'An unknown error occurred!'});
+  res.json({ message: error.message || "An unknown error occurred!" });
 });
 
-app.listen(5000);
+mongoose
+  .connect('mongodb+srv://gab:gab123@cluster0.owcv8h6.mongodb.net/places?retryWrites=true&w=majority')
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch(err => {
+    console.log(err)
+  });
--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,6 +1,6 @@
-const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
+const User = require("../models/user");
 
 const DUMMY_USERS = [
   {
@@ -11,43 +11,86 @@ const DUMMY_USERS = [
   },
 ];
 
-const getUsers = (req, res, next) => {
-  res.json({ users: DUMMY_USERS });
+const getUsers = async (req, res, next) => {
+  let users;
+  try{
+    users = await User.find({}, '-password');
+  }
+  catch(err){
+    const error = new HttpError('Fetching users failed, please try again later.', 500);
+    return next(error);
+  }
+  res.json({users: users.map(user => user.toObject({getters: true}))});
+
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+    return next(HttpError("Invalid inputs passed, please check your data.", 422)); 
   }
 
-  const { name, email, password } = req.body;
+  const { name, email, password, places} = req.body;
 
-  const hasUser = DUMMY_USERS.find((u) => u.email === email);
-  if (hasUser) {
-    throw new HttpError("Could not create user, email already exists.", 422);
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      "Signing up failed, please try again later.",
+      500
+    );
+    return next(error);
   }
 
-  const createdUser = {
-    id: uuidv4(),
+  if (existingUser) {
+    const error = new HttpError(
+      "User exists already, please login instead.",
+      422
+    );
+    return next(error);
+  }
+
+  const createdUser = new User({
     name,
     email,
+    image:
+      "https://media.istockphoto.com/photos/new-york-city-skyline-picture-id486334510?b=1&k=20&m=486334510&s=170667a&w=0&h=QN8zZ6h4u8blAxXQlc1L1Nb3WGtN82_dXcbiDnfylPI=",
     password,
-  };
+    places
+  });
 
-  DUMMY_USERS.push(createdUser);
-  res.status(201).json({ user: createdUser });
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError("Signing up failed, please try again.", 500);
+    return next(error);
+  }
+
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError(
-      "Could not identify user, credentials seem to be wrong.",
-      401
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      "Loggin in failed, please try again later.",
+      500
     );
+    return next(error);
+  }
+
+  if(!existingUser || existingUser.password !== password){
+    const error = new HttpError(
+      'Invalid credentials, could not log you in.', 401
+    );
+    return next(error);
   }
 
   res.json({ message: "Logged in!" });
